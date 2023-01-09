c41f792f5608d906b4f2013f915d04dd319090af
Manuel Carrasquero <maleca1993@gmail.com>
2023-01-09 15:52:31 -0400

    Comentario

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -3,8 +3,17 @@ from django.http import HttpResponse
 from AppCoder.models import Curso
 
 # Create your views here.
-def curso(self):
-    curso=Curso(nombre="Desarrollo web", camada="19881")
-    curso.save()
-    documentoDeTexto=f"--->Curso:{curso.nombre} Camada:{curso.camada}"
-    return HttpResponse(documentoDeTexto)
+def inicio(request):
+    return render(request, "AppCoder/inicio.html")
+
+def cursos(request):
+    return render(request, "AppCoder/cursos.html")
+
+def profesores(request):
+    return render(request, "AppCoder/profesores.html")
+
+def estudiantes(request):
+    return render(request, "AppCoder/estudiantes.html")
+
+def entregables(request):
+    return render(request, "AppCoder/entregables.html")
